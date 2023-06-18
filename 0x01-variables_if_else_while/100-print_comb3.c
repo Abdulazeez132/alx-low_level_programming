bebdb21fd4ab83ded3a2678fd7f63a24790a722b
Abdulazeez <ibrahimabdulazeez132@gmail.com>
2023-06-18 00:48:10 -0700

    two digits comvo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,19 +14,17 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			if (i != j)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
+			putchar(i + '0');
+			putchar(j + '0');
 
-				if (i != 8 && j != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			if (i == 8 && j == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+
 		}
 	}
 	putchar('\n');
