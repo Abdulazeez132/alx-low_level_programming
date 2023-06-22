317d958ed636557712c097589defa45b2d5ccfc6
Abdulazeez <ibrahimabdulazeez132@gmail.com>
2023-06-22 07:49:40 +0100

    c prog

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,18 +13,13 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-	{
-		last_digit = -n % 10;
-		_putchar(last_digit);
+		n = n;
 
-		return (last_digit);
-	}
+	last_digit = n % 10;
 
-	else
-	{
-		last_digit = n % 10;
-		_putchar(last_digit);
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-		return (last_digit);
-	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
