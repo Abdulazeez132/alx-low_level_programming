0a256855e37864d84dec44d3e0c09e857fc241c0
Abdulazeez <ibrahimabdulazeez132@gmail.com>
2023-06-28 08:26:19 +0100

    pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of string
+ *
+ * @s: string to measure
+ *
+ * Return: returns the lenth of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while (*s++)
+		i++;
+}
