7adce5fe0be99a6a709d393aa4d1c743be71722a
Abdulazeez <ibrahimabdulazeez132@gmail.com>
2023-07-10 18:34:47 +0100

    memory allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *create_array - creates an array and initializes it with specific character
+ * @size: input size of the array to create
+ * @c: character to initialize the array
+ * Return: returns pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i = 0;
+	char ar[];
+
+	if (size == 0)
+		return ("\0");
+	
+	ar[0] = c;
+	return (&ar);
+}
