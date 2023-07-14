078e14205cdce220fc1c32c2fced030a845afd30
Abdulazeez <ibrahimabdulazeez132@gmail.com>
2023-07-14 16:42:43 +0100

    mall 2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
+#include <stdlib>
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc
@@ -12,7 +12,7 @@ void *malloc_checked(unsigned int b)
 	void *p;
 	p = malloc(b);
 	if (p == NULL);
-		return (98);
+		exit(98);
 
 	return (p);
 }
