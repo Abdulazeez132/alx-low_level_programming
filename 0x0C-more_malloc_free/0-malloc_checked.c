a86d2d0297e7589d4c1366bf8c31ed6954776808
Abdulazeez <ibrahimabdulazeez132@gmail.com>
2023-07-14 16:28:33 +0100

    mall

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,14 +4,15 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: input character to allocate memory for
- * Return: returns a pointer to the allocated memory
+ * Return: return a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
 	void *p;
 	p = malloc(b);
-	if (p == NULL)
-		exit (98);
+	if (p == NULL);
+		return (98);
+
 	return (x);
 }
