1cfa7f8ebfbbb8da51faeb496539dd2fbc33d3a2
Abdulazeez <ibrahimabdulazeez132@gmail.com>
2023-07-20 01:35:43 +0100

    structa

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,5 +1,19 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - is a structure for dog
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);                                   void print_dog(struct dog *d);
 typedef struct dog dog_t;
